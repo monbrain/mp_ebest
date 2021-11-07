c1ae43759dd487ff3a9d7ea2cbfce45a1bd24e89
hopelife <deverlife@gmail.com>
2021-11-07 17:32:40 +0900

    _session.py

--- mp_ebest/_session.py
+++ mp_ebest/_session.py
@@ -1,8 +1,8 @@
 # -*- coding=utf-8 -*-
 """
 주요 기능: 
-    - ebest XING-API 로그인
-    - 
+    - ebest XINGAPI 접속
+    - 계좌별 로그인(동일 계좌 접속 세션 존재시 재접속 안함)
 
 사용례: 
     - 
@@ -15,6 +15,7 @@
 ##------------------------------------------------------------
 import os, sys
 import time
+import json
 
 ##@@ Package 모듈
 ##------------------------------------------------------------
@@ -24,87 +25,98 @@ from win32com.client import DispatchWithEvents
 
 ##@@ User 모듈
 ##------------------------------------------------------------
-sys.path.append(os.path.join(os.path.dirname(__file__), '../../_public'))
-from utils_basic import (
-    _read_file, 
-    _write_file, 
-    _create_folder,
-    _project_dicts
-)
-
-from utils_xlsx import (
-    read_xsheet, 
-    write_xsheet,
-)
-
-sys.path.append(os.path.join(os.path.dirname(__file__), '../_config'))
-from configs import (
-    # ACCOUNT_EBEST, 
-    _account
-)
 
 ##@@@ 전역 상수/변수
 ##============================================================
+# ACNTS_PATH = "./settings/accounts.yml"
+ACNTS_PATH = "./settings/accounts.json"
 
-# ##@@ path, 
-# ##------------------------------------------------------------
-
-# ## 계정account 정보가 있는 폴더
-# CONIFIG_FOLDER = os.path.join(os.path.abspath('../_config/')).replace("\\", "/")
-
-# ##@@@ 보조 함수
+# ##@@@ Private Class/function
 # ##============================================================
 
-# ##@@ 아이디/비밀번호 설정
+# ##@@ 계정/계좌 설정
 # ##------------------------------------------------------------
+def _load_acnts(path=ACNTS_PATH):
+    """[계정/계좌 설정]
 
-# def _set_login(mode="DEMO", division="주식"):
-#     return globals()[f"ACCOUNT_EBEST"][mode]  ## TODO: ACCOUNT_{division}_{agency.upper()}
+    Args:
+        acntNo (str): 계좌 번호
+        acnts (dict, optional): 계정/계좌 정보. Defaults to {}.
 
+    Returns:
+        [dict]: 계정/계좌 정보
+    """
+    with open(path, "r", encoding="utf-8") as f:
+        return json.load(f)
+    # with open(path, "r", encoding="utf-8") as f:
+    #     return yaml.load(f, Loader=yaml.FullLoader)
+
+
+def _get_account(acntNo, acnts={}):
+    """[계정/계좌 설정]
+
+    Args:
+        acntNo (str): 계좌 번호
+        acnts (dict, optional): 계정/계좌 정보. Defaults to {}.
+
+    Returns:
+        [dict]: 계정/계좌 정보
+    """
+    return acnts[acntNo]
+
+
+##@@@ Public Class/function
+##============================================================
 
 ##@@ 세션 핸들러
 ##------------------------------------------------------------
 class _SessionHandler:
     def OnLogin(self, code, msg):
-        """ 서버와의 로그인이 끝나면 실행되는 함수
-            @arg code[str] 서버에서 받은 메시지 코드
-            @arg msg[str] 서버에서 받은 메시지 정보
+        """서버 접속시 실행
+
+        Args:
+            code (str): 서버에서 받은 메시지 코드
+            msg (str): 서버에서 받은 메시지 정보
         """
         self.waiting = False
-        # self.waiting = True
     
         if code == '0000':
             print('[*] 로그인 성공')
         else:
             print(f'[*] 로그인 실패 : {msg}')
-    
+            exit()
+
     def OnDisconnect(self):
-        """ 서버와의 연결이 끊어졌을 때 실행되는 함수
+        """ 서버 접속 종료시 실행
         """
         self.waiting = False
         print('[*] 서버와의 연결이 끊어졌습니다')
 
 
-##@@@ 실행 함수
-##============================================================
-
 ##@@ 세션
 ##------------------------------------------------------------
-
 class Session():
     _session = DispatchWithEvents('XA_Session.XASession', _SessionHandler)
-    def __init__(self, mode="DEMO"):
-        """ 로그인
+
+    def __init__(self, acntNo="55501071053", acnts=ACNTS_PATH):
+        """Session 클래스 생성시 실행
+
+        Args:
+            acntNo (str, optional): 계좌 번호. Defaults to "55501071053".
+            acnts (str|dict, optional): str: 계좌/계정 정보 파일(.json) 경로. dict: 계좌/계정 정보
+            TODO: private package에서 dict로 직접 설정 예정!!!
         """
-        # NOTE: 동일 mode의 연결이 있으면 pass
-        if self._session.IsConnected() and self._session.mode == mode:
-            # print(f"이미 '{mode}'로 로그인 중입니다.")
+        acnts = _load_acnts(path=acnts) if not acnts or type(acnts) == str else acnts
+
+        # NOTE: 동일 계좌 연결이 있으면 pass
+        if self._session.IsConnected() and self._session.acntNo == acntNo:
+            # print(f"이미 '{acntNo}'로 로그인 중입니다.")
             pass
             # self._session.DisconnectServer()
         else:
-            config = _account(mode=mode)
+            account = _get_account(acntNo, acnts)
             if self._session.IsConnected():
-                print(f"로그인 모드를 '{self._session.mode}'에서 '{mode}'로 변경합니다.")
+                print(f"연결 계좌를 '{self._session.acntNo}'에서 '{acntNo}'로 변경합니다.")
 
             # 로그인
             print(f"ebest로 로그인합니다.")
@@ -112,36 +124,27 @@ class Session():
                 'r': 'hts.ebestsec.co.kr',
                 'd': 'demo.ebestsec.co.kr',
                 'a': '127.0.0.1'
-            }.get(mode.lower()[:1], 'demo.ebestsec.co.kr')
+            }.get(account['mode'].lower()[:1], 'demo.ebestsec.co.kr')
             port = 20001
-            cert = '' if url == 'demo.ebestsec.co.kr' else config['cert']
+            cert = '' if url == 'demo.ebestsec.co.kr' else account['cert']
 
-            print(f"{mode}, {config}")
-            # 로그인 요청을 보낸다
-            self._session.mode = mode  # NOTE: 동일 모드 로그인 확인용
-            # self._session.id = id  # NOTE: 동일 아이디 로그인 확인용
+            print(f"{acntNo}, {account}")
+            
+            # 로그인 요청
+            self._session.acntNo = acntNo  # NOTE: 동일 계좌 여부 확인용
             self._session.waiting = True
             self._session.ConnectServer(url, port)
-            self._session.Login(config['id'], config['pw'], cert, 0, 0)
+            self._session.Login(account['id'], account['pw'], cert, 0, 0)
+
+            self.account = account  # TODO: 보안을 위해 삭제 예정!!, NOTE: 세션 정보 확인용
 
             while self._session.waiting:
                 pythoncom.PumpWaitingMessages()
-
+        
 
 if __name__ == "__main__":
-    pass
-    s1 = Session(mode="DEMO")
-    time.sleep(2)
-    print(f"{id(s1)}")
-    s1 = Session(mode="REAL")
-    time.sleep(2)
-    print(f"{id(s1)}")
-    s1 = Session(mode="REAL")
-    time.sleep(2)
-    print(f"{id(s1)}")
-    s1 = Session(mode="ACE")
-    print(f"{id(s1)}")
-    
-    # l = _set_login(mode='DEMO', division='주식')
-    # l = _set_login_xlsx(mode='DEMO', division='주식')
-    # print(l)
+    acnts = _load_acnts()
+    print(acnts)
+
+    # with open(ACNTS_PATH, 'w', encoding='utf8') as f:
+    #     json.dump(acnts, f, ensure_ascii=False, indent=4)
--- setup.py
+++ setup.py
@@ -12,6 +12,11 @@ setup(
     packages=['mp_ebest'],
     # entry_points={'console_scripts': ['mp_ebest = mp_ebest.__main__:main']},
     keywords='ebest xingapi',
-    # python_requires='~=3.3',
+    # python_requires='>=3.8',  # Python 3.8.6-32 bit
+    # install_requires=[ # 패키지 사용을 위해 필요한 추가 설치 패키지
+    #     'pythoncom', 
+    #     'win32com'
+    # ],
     # zip_safe=False
 )
+
